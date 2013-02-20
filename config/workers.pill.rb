ac86397fa6c5f79e6c20b9a07f8335329ac67549
Yury Kotov <bairkan@gmail.com>
2013-02-20 15:34:54 +0400

    Запуск процесса воркера с правами пользователя

--- config/workers.pill.rb
+++ config/workers.pill.rb
@@ -8,12 +8,15 @@ app_config = YAML::load(File.open(app_cfg_path, 'r'))
 config = YAML::load File.open(File.join(working_dir, app_config['workers_config']), 'r')
 
 Bluepill.application('mp_live_transcoder', foreground: false, log_file: "#{working_dir}/log/bluepill.log") do |app|
-  config['workers'].each_with_index do |worker, index|
+  config['workers'].each_with_index do |command, index|
     app.process("worker_#{index}") do |process|
-      process.start_command = worker['command']
+      process.start_command = command
 
       process.pid_file = "#{working_dir}/tmp/worker_#{index}.pid"
       process.working_dir = "#{working_dir}"
+
+      process.uid = 'deployer'
+      process.gid = 'deployer'
 
       process.daemonize = true
     end
